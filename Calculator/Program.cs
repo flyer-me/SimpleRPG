56f58415dd0d338da9807df110c86d731358f442
flyer-me <flyer558@163.com>
2024-03-10 10:09:15 +0800

    a excel tool.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,106 +1,88 @@
-﻿class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
+﻿using OfficeOpenXml;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        bool endApp = false;
-        // Display title as the C# console calculator app.
-        Console.WriteLine("Console Calculator in C#\r");
-        Console.WriteLine("------------------------\n");
-
-        while (!endApp)
+        Console.WriteLine("街乡平台设备数据情况比对程序\n\n数据表文件名必须以'街乡平台设备在线'开头，以'.xlsx'结尾,且在线原数据位于第二个sheet;\n\n统计结果文件名为'小区统计表1775.xlsx',不能更改");
+        var file_list = Directory.GetFiles(".", "街乡平台设备在线*.xlsx");
+        if (file_list.Length == 0)
         {
-            // Declare variables and set to empty.
-            string numInput1 = "";
-            string numInput2 = "";
-            double result = 0;
+            Console.WriteLine("未找到'街乡平台设备在线'开头的文件，按enter退出...");
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
 
-            // Ask the user to type the first number.
-            Console.Write("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
+        var sum = new Dictionary<string, int>();
+        var online = new Dictionary<string, int>();
+        var not_online = new Dictionary<string, int>();
 
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
+        foreach (var file_name in file_list)
+        {
+            using (var package = new ExcelPackage(new FileInfo(file_name)))
             {
-                Console.Write("This is not valid input. Please enter an integer value: ");
-                numInput1 = Console.ReadLine();
+                var workbook = package.Workbook;
+                var sheet = workbook.Worksheets[1];
+                for (int row = 2; row <= sheet.Dimension.End.Row; row++)
+                {
+                    var category = sheet.Cells[row, 2].Text.Split('/').Last();
+                    var status = sheet.Cells[row, 4].Text;
+                    if (!sum.ContainsKey(category))
+                    {
+                        sum[category] = 0;
+                    }
+                    sum[category] += 1;
+                    if (!online.ContainsKey(category))
+                    {
+                        online[category] = 0;
+                    }
+                    if (!not_online.ContainsKey(category))
+                    {
+                        not_online[category] = 0;
+                    }
+                    if (status == "在线" || status == "未检测")
+                    {
+                        online[category] = online.GetValueOrDefault(category, 0) + 1;
+                    }
+                    else if (status == "离线" && sheet.Cells[row, 7].Text != "0")
+                    {
+                        not_online[category] = not_online.GetValueOrDefault(category, 0) + 1;
+                    }
+                }
             }
+        }
+        Console.WriteLine("统计完成..");
 
-            // Ask the user to type the second number.
-            Console.Write("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
+        using (var file = new ExcelPackage(new FileInfo("小区统计表1775.xlsx")))
+        {
+            var sheet = file.Workbook.Worksheets[0];
 
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
+            for (int row = 2; row <= sheet.Dimension.End.Row; row++)
             {
-                Console.Write("This is not valid input. Please enter an integer value: ");
-                numInput2 = Console.ReadLine();
+                var value = sheet.Cells[row, 5].Text;
+                if (online.ContainsKey(value))
+                {
+                    sheet.Cells[row, 11].Value = online[value];
+                    sheet.Cells[row, 12].Value = not_online[value];
+                    sheet.Cells[row, 13].Value = sum[value];
+                }
+                else
+                {
+                    sheet.Cells[row, 11].Value = "平台无设备";
+                }
             }
-
-            // Ask the user to choose an operator.
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string op = Console.ReadLine();
-
             try
             {
-                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                if (double.IsNaN(result))
-                {
-                    Console.WriteLine("This operation will result in a mathematical error.\n");
-                }
-                else Console.WriteLine("Your result: {0:0.##}\n", result);
+                file.Save();
             }
-            catch (Exception e)
+            catch (IOException)
             {
-                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                Console.WriteLine("结果写入失败,请关闭文件后重试");
+                Environment.Exit(1);
             }
-
-            Console.WriteLine("------------------------\n");
-
-            // Wait for the user to respond before closing.
-            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-            if (Console.ReadLine() == "n") endApp = true;
-
-            Console.WriteLine("\n"); // Friendly linespacing.
         }
-        return;
+        Console.WriteLine("结果写入成功,按enter退出...");
+        Console.ReadLine();
     }
 }
+
