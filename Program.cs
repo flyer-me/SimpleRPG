094ce092b91d38f1e91c4cc9f098e469d1cd3199
flyer <flyer558@163.com>
2024-03-26 17:16:29 +0800

    add main Program helloworld

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnetHello;
+using System;
+class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string line;
+        while ((line = Console.ReadLine()) != null) { // 注意 while 处理多个 case
+            string[] tokens = line.Split(" ");
+            Console.WriteLine(tokens.Last().Length);
+        }
+    }
+}
