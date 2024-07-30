8163dff11fe0024cedfa8e84ad5a3ef0d1aab9ca
flyer-me <wup3306@gmail.com>
2024-07-30 21:31:49 +0800

    添加Location类，以及在WPF中引用Image文件

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,6 +9,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -18,6 +19,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Assets = 10000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your home.";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
         }
     }
 }
