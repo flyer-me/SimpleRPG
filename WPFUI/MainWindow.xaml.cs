865b61334edfcceacd7269ade8c2e1e16278330b
flyer-me <wup3306@gmail.com>
2024-08-01 22:38:45 +0800

    添加世界移动按钮，使用XAML ToVisibilityConverter限制输入，添加OnPropertyChanged事件处理

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System.Text;
+﻿using Engine.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Engine.ViewModels;
 
 namespace WPFUI
 {
@@ -27,10 +18,24 @@ namespace WPFUI
             DataContext = _gameSession;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_MoveNorth(object sender, RoutedEventArgs e)
         {
-            _gameSession.CurrentPlayer.ExperiencePoints += 15;
+            _gameSession.MoveNorth();
+        }
 
+        private void Button_Click_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+
+        private void Button_Click_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+
+        private void Button_Click_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
         }
     }
 }
