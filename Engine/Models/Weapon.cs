118e9baf3d62c0b10f26c11ce6fde19ceeb3e684
flyer-me <wup3306@gmail.com>
2024-08-03 22:26:49 +0800

    创建Item类和工厂类

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; private set; }
+        public int MaximumDamage { get; private set; }
+        public Weapon(int itemTypeID, string name, int price, int minimumDamage, int maximumDamage) 
+            : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
