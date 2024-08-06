2e2ed4b91548e1dcab9ba94aed61b391262226ec
flyer-me <wup3306@gmail.com>
2024-08-06 22:59:16 +0800

    增加<怪物>类和工厂类

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -16,13 +16,12 @@ namespace Engine.Factories
 
             _standardGameItems.Add(new Weapon(1001, "木矛", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "锈剑", 5, 1, 3));
-            
             _standardGameItems.Add(new GameItem(9001, "蛇牙", 1));
             _standardGameItems.Add(new GameItem(9002, "蛇皮", 2));
-            _standardGameItems.Add(new GameItem(9003, "Rat tail", 1));
-            _standardGameItems.Add(new GameItem(9004, "Rat fur", 2));
-            _standardGameItems.Add(new GameItem(9005, "Spider fang", 1));
-            _standardGameItems.Add(new GameItem(9006, "Spider silk", 2));
+            _standardGameItems.Add(new GameItem(9003, "鼠尾", 1));
+            _standardGameItems.Add(new GameItem(9004, "鼠毛", 2));
+            _standardGameItems.Add(new GameItem(9005, "蜘蛛牙", 1));
+            _standardGameItems.Add(new GameItem(9006, "蛛丝", 2));
         }
 
         public static GameItem CreateGameItem(int itemTypeID)
