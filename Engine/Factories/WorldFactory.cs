8681ef64fc86e2c52f7b90bcf2d76904be4eb4d6
flyer <flyer558@163.com>
2024-08-12 16:45:08 +0800

    将部分变量设置为readonly,并使用字符串拼接减少重复字符串

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -12,43 +12,43 @@ namespace Engine.Factories
         internal static World CreateWorld()
         {
             World newWorld = new World();
-            newWorld.AddLocation(0, -1, "Home", "This is your home.", "pack://application:,,,/Engine;component/Images/Locations/Home.png");
+            newWorld.AddLocation(0, -1, "Home", "This is your home.", "Home.png");
 
             newWorld.AddLocation(-2, -1, "农田",
                "There are rows of corn growing here, with giant rats hiding between them.",
-               "pack://application:,,,/Engine;component/Images/Locations/FarmFields.png");
+               "FarmFields.png");
 
             newWorld.LocationAt(-2, -1).UpdateMonsterEncounter(2, 100);
 
             newWorld.AddLocation(-1, -1, "Farmer's House",
                 "This is the house of your neighbor, Farmer Ted.",
-                "pack://application:,,,/Engine;component/Images/Locations/Farmhouse.png");
+                "Farmhouse.png");
 
             newWorld.AddLocation(-1, 0, "Trading Shop",
                 "The shop of Susan, the trader.",
-                "pack://application:,,,/Engine;component/Images/Locations/Trader.png");
+                "Trader.png");
 
             newWorld.AddLocation(0, 0, "Town square",
                 "You see a fountain here.",
-                "pack://application:,,,/Engine;component/Images/Locations/TownSquare.png");
+                "TownSquare.png");
 
             newWorld.AddLocation(1, 0, "Town Gate",
                 "There is a gate here, protecting the town from giant spiders.",
-                "pack://application:,,,/Engine;component/Images/Locations/TownGate.png");
+                "TownGate.png");
 
             newWorld.AddLocation(2, 0, "蜘蛛森林",
                 "The trees in this forest are covered with spider webs.",
-                "pack://application:,,,/Engine;component/Images/Locations/SpiderForest.png");
+                "SpiderForest.png");
 
             newWorld.LocationAt(2, 0).UpdateMonsterEncounter(3, 100);
 
             newWorld.AddLocation(0, 1, "Herbalist's hut",
                 "You see a small hut, with plants drying from the roof.",
-                "pack://application:,,,/Engine;component/Images/Locations/HerbalistsHut.png");
+                "HerbalistsHut.png");
 
             newWorld.AddLocation(0, 2, "草药师的花园",
                 "草木茂盛，有蛇盘踞其中。",
-                "pack://application:,,,/Engine;component/Images/Locations/HerbalistsGarden.png");
+                "HerbalistsGarden.png");
 
             newWorld.LocationAt(0, 2).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(1));
             newWorld.LocationAt(0, 2).UpdateMonsterEncounter(1, 100);
--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -33,7 +33,7 @@ namespace Engine.Models
             int rewardExperiencePoints, int rewardAssets)
         {
             Name = name;
-            ImageName = string.Format("pack://application:,,,/Engine;component/Images/Monsters/{0}", imageName);
+            ImageName = $"pack://application:,,,/Engine;component/Images/Monsters/{imageName}";
             MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
             MinimumDamage = minimumDamage;
