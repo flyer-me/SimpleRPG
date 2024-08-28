c8130461073466105889ad527206d07bc67e5b02
flyer <flyer558@163.com>
2024-08-28 16:06:15 +0800

    增加MSTest单元测试

---
+++ TestEngine/ViewModels/TestGameSession.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine.ViewModels;
+
+namespace TestEngine.ViewModels
+{
+    [TestClass]
+    public class TestGameSession
+    {
+        [TestMethod]
+        public void TestCreateGameSession()
+        {
+            GameSession gameSession = new GameSession();
+            Assert.IsNotNull(gameSession.CurrentPlayer);
+            Assert.AreEqual("Town square", gameSession.CurrentLocation.Name);
+        }
+        [TestMethod]
+        public void TestPlayerMovesHomeAndIsCompletelyHealedOnKilled()
+        {
+            GameSession gameSession = new GameSession();
+            gameSession.CurrentPlayer.TakeDamage(int.MaxValue);
+            Assert.AreEqual("Home", gameSession.CurrentLocation.Name);
+            Assert.AreEqual(gameSession.CurrentPlayer.Level * 10, gameSession.CurrentPlayer.CurrentHitPoints);
+        }
+    }
+}
