0f5a5f08934d8bc4dd99e26cca35ab17d067a0fc
flyer-me <wup3306@gmail.com>
2024-08-30 10:59:19 +0800

    添加食谱recipes

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -10,7 +10,7 @@ namespace Engine.Models
     public class Player : LivingEntity
     {
         #region Properties
-        private string _characterClass;
+        private string _characterClass = String.Empty;
         private int _experiencePoints;
         public string CharacterClass
         {
@@ -32,6 +32,7 @@ namespace Engine.Models
             }
         }
         public ObservableCollection<QuestStatus> Quests { get; }
+        public ObservableCollection<Recipe> Recipes { get; }
         #endregion
         public event EventHandler OnLeveledUp;
         public Player(string name, string characterClass, int experiencePoints,
@@ -41,6 +42,7 @@ namespace Engine.Models
             CharacterClass = characterClass;
             ExperiencePoints = experiencePoints;
             Quests = new ObservableCollection<QuestStatus>();
+            Recipes = new ObservableCollection<Recipe>();
         }
         public bool HasAllTheseItems(List<ItemQuantity> items)
         {
@@ -56,6 +58,13 @@ namespace Engine.Models
          public void AddExperience(int experiencePoints)
         {
             ExperiencePoints += experiencePoints;
+        }
+        public void LearnRecipe(Recipe recipe)
+        {
+            if (!Recipes.Any(r => r.ID == recipe.ID))
+            {
+                Recipes.Add(recipe);
+            }
         }
         private void SetLevelAndMaximumHitPoints()
         {
